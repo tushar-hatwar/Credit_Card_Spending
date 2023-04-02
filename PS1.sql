eda0dd68d94700405dd5ec2b8ca50bb47e0b7197
Tushar Hatwar <60131764+tushar-hatwar@users.noreply.github.com>
2023-04-02 14:54:44 +0530

    Update PS1.sql

--- PS1.sql
+++ PS1.sql
@@ -3,7 +3,6 @@ with cte as
 SELECT city, sum(amount) as spend
 FROM  credit_card_transcations
 group by city
---order by spend desc
 )
 SELECT *, sum(spend) over (order by spend desc rows between UNBOUNDED PRECEDING and UNBOUNDED FOLLOWING )as total_spend,
 ROUND((spend /sum(spend) over (order by spend desc rows between UNBOUNDED PRECEDING and UNBOUNDED FOLLOWING )*100),2) as percentage_contribution
